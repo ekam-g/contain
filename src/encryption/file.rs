f82366d97c274f1044aa516f77ad9ba06fb2ce5e
Ekam <carghai74@gmail.com>
2024-01-01 14:29:17 -0500

    stufff

---
+++ src/encryption/file.rs
@@ -0,0 +1,23 @@
+use std::{path::{Path, PathBuf}};
+
+
+
+pub fn encrypt_file() {
+    let path = Path::new(".").display();
+
+}
+
+pub fn decrypt_file() {}
+
+pub fn file_location() -> PathBuf {
+    let mut home_dir = match home::home_dir() {
+        Some(path) => path,
+        None => {
+            println!("Failed to get your home dir!");
+            PathBuf::new()
+        },
+    };
+    home_dir.push(".time-lock");
+    home_dir.set_extension(".contain");
+    home_dir
+}
