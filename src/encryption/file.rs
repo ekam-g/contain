f970a53a1cfcfb40a39276ad1fcd70a06f976129
Ekam <carghai74@gmail.com>
2024-01-01 17:44:34 -0500

    stuff

--- src/encryption/file.rs
+++ src/encryption/file.rs
@@ -1,13 +1,36 @@
-use std::path::{Path, PathBuf};
+use std::io::{BufReader, Read, Seek, Write, SeekFrom};
+use std::path::PathBuf;
+use std::fs::{File, OpenOptions};
 
+use super::base::{encrypt, decrypt};
 
-
-pub fn encrypt_file() {
-    let _path = Path::new(".").display();
-
+fn read_file(path: PathBuf) -> Result<(File, String), std::io::Error>{
+    let mut file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open(path)?;
+    let mut buf_reader = BufReader::new(file);
+    let mut data = String::new();
+    buf_reader.read_to_string(&mut data)?;
+    Ok((file, data))
 }
 
-pub fn decrypt_file() {}
+pub fn encrypt_file(path : PathBuf) -> Result<File, std::io::Error>{
+    let (mut file,mut data) = read_file(path)?;
+    data = encrypt(data.as_bytes());
+    file.seek(SeekFrom::Start(0)).unwrap();
+    file.write_all(data.as_bytes()).unwrap();
+    Ok(file)
+}
+
+pub fn decrypt_file(path: PathBuf) -> Result<File, Box<dyn std::error::Error>> {
+    let (mut file,data) = read_file(path)?;
+    let data = decrypt(&data)?;
+    file.seek(SeekFrom::Start(0)).unwrap();
+    file.write_all(&data).unwrap();
+    Ok(file)
+}
 
 pub fn file_location() -> PathBuf {
     let mut home_dir = match home::home_dir() {
@@ -18,6 +41,6 @@ pub fn file_location() -> PathBuf {
         },
     };
     home_dir.push(".time-lock");
-    home_dir.set_extension(".contain");
+    home_dir.set_extension("contain");
     home_dir
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use encryption::file::encrypt_file;
+
 pub mod encryption;
 
 fn main() {
     encryption::test();
+    encrypt_file();
 }
