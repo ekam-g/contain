372a7bbf9152284b9b07313555379cf5e6853a96
carghai <carghai74@gmail.com>
2024-03-14 12:33:49 -0400

    clean, doen't work yet

--- src/encryption/base.rs
+++ src/encryption/base.rs
@@ -1,29 +1,10 @@
-use std::{error::Error};
 use std::str;
-use aead::{generic_array::{GenericArray}, Aead, AeadCore, KeyInit};
-use aes_gcm::{Aes256Gcm};
+use aead::{generic_array::GenericArray, Aead, AeadCore, KeyInit};
+use aes_gcm::Aes256Gcm;
 use const_random::const_random;
 use anyhow::anyhow;
 
 pub const RANDOM_BYTES: [u8; 32] = random();
-
-
-/// orig must be a string of the form [hexNonce]/[hexCipherText]/[hexMac]. This
-/// is the data returned from encrypt(). This function splits the data, removes
-/// the hex encoding, and returns each as a list of bytes.
-fn split_iv_data_mac(orig: &str) -> Result<(Vec<u8>, Vec<u8>, Vec<u8>), Box<dyn Error>> {
-    let split: Vec<&str> = orig.split('/').collect();
-
-    if split.len() != 3 {
-        return Err(Box::new(std::io::Error::new(std::io::ErrorKind::Other, "Invalid input format")));
-    }
-
-    let iv = hex::decode(split[0])?;
-    let data = hex::decode(split[1])?;
-    let mac = hex::decode(split[2])?;
-
-    Ok((iv, data, mac))
-}
 
 /// gets a valid key. This must be exactly 16 bytes. if less than 16 bytes, it will be padded with 0.
 /// If more than 16 bytes, it will be truncated
@@ -54,7 +35,7 @@ pub fn decrypt(cipher_text: &[u8], key: &[u8]) -> anyhow::Result<Vec<u8>> {
 }
 pub fn example() {
     let data = "Yo yo if this works your lit homie";
-    let key = get_valid_key("Hello TEST key");
+    let key = get_valid_key("Hello TEST key IHBGEJHFBWOHDFBJSHDBFJHASDBFJHASJHDBsspp>");
     println!("Data to encrypt: \"{}\"", &data);
 
     println!("Encrypting now");
