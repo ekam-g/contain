ac547dcf9954f892f83602166f67c635ca99bb68
carghai <carghai74@gmail.com>
2024-03-15 12:02:47 -0400

    needs rewrite

--- src/encryption/file.rs
+++ src/encryption/file.rs
@@ -1,55 +1,61 @@
-use std::io::{BufReader, Read, Seek, Write, SeekFrom};
-use std::path::PathBuf;
-use std::fs::{File, OpenOptions};
+use super::base::{decrypt, encrypt};
 use crate::encryption::base::KEY;
-use super::base::{encrypt, decrypt};
+use std::fs::{File, OpenOptions};
+use std::io::{BufReader, Read, Seek, SeekFrom, Write};
+use std::path::PathBuf;
 
-
-fn read_file(path: PathBuf) -> Result<(File, Vec<u8>), std::io::Error> {
-    let file = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(path)?;
-
-    let mut buf_reader = BufReader::new(&file);
-    let mut data: Vec<u8> = Vec::new();
-    buf_reader.read_to_end(&mut data)?;
-
-    Ok((file, data))
+struct EncryptedFile {
+    m_file : File
 }
+impl EncryptedFile {
+    pub fn new(path: PathBuf) -> anyhow::Result<Self> {
+        Self {
+            m_file : Self::read_file(path)?,
+        }
+    }
+    fn read_file(path: PathBuf) -> Result<(File, Vec<u8>), std::io::Error> {
+        let file = OpenOptions::new().read(true).write(true).open(path)?;
 
-pub fn encrypt_file(path : PathBuf) -> anyhow::Result<File>{
-    let (mut file, data) = read_file(path)?;
-    let encrypted_data = encrypt(&data, KEY.as_ref())?;
-    drop(data);
-    file.seek(SeekFrom::Start(0)).unwrap();
-    file.set_len(0)?;
-    file.write_all(&encrypted_data).unwrap();
-    file.flush()?;
-    Ok(file)
-}
+        let mut buf_reader = BufReader::new(&file);
+        let mut data: Vec<u8> = Vec::new();
+        buf_reader.read_to_end(&mut data)?;
 
-pub fn decrypt_file(path: PathBuf) -> anyhow::Result<File> {
-    let (mut file,data) = read_file(path)?;
-    let data =decrypt(&data, KEY.as_ref())?;
-    file.seek(SeekFrom::Start(0)).unwrap();
-    file.set_len(0)?;
-    file.write_all(&data).unwrap();
-    file.flush()?;
-    Ok(file)
-}
+        Ok((file, data))
+    }
 
-pub fn file_location() -> PathBuf {
-    let mut home_dir = match home::home_dir() {
-        Some(path) => path,
-        None => {
-            println!("Failed to get your home dir!");
-            PathBuf::new()
-        },
-    };
-    home_dir.push(".time-lock");
-    home_dir.set_extension("contain");
-    home_dir
+    pub fn encrypt_file(path: PathBuf) -> anyhow::Result<File> {
+        let (mut file, data) = EncryptedFile::read_file(path)?;
+        let encrypted_data = encrypt(&data, KEY.as_ref())?;
+        drop(data);
+        file.seek(SeekFrom::Start(0)).unwrap();
+        file.set_len(0)?;
+        file.write_all(&encrypted_data).unwrap();
+        file.flush()?;
+        Ok(file)
+    }
+
+    pub fn decrypt_file(path: PathBuf) -> anyhow::Result<File> {
+        let (mut file, data) = EncryptedFile::read_file(path)?;
+        let data = decrypt(&data, KEY.as_ref())?;
+        file.seek(SeekFrom::Start(0)).unwrap();
+        file.set_len(0)?;
+        file.write_all(&data).unwrap();
+        file.flush()?;
+        Ok(file)
+    }
+
+    pub fn file_location() -> PathBuf {
+        let mut home_dir = match home::home_dir() {
+            Some(path) => path,
+            None => {
+                println!("Failed to get your home dir!");
+                PathBuf::new()
+            }
+        };
+        home_dir.push(".time-lock");
+        home_dir.set_extension("contain");
+        home_dir
+    }
 }
 #[test]
 pub fn example() {
