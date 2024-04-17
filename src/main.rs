3a512f212931c981285d2fd895085b12d1436c43
carghai <carghai74@gmail.com>
2024-04-17 00:41:43 -0400

    base

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
 // #![feature(const_for)]
 pub mod time_manger;
 pub mod encryption;
+use rfd::FileDialog;
 
 slint::include_modules!();
-fn main() {
-    
-    MyApp::new().unwrap().run().unwrap();
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = MyApp::new()?;
+
+    ui.on_request_open_file({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            let files = FileDialog::new()
+                .add_filter("text", &["txt", "rs"])
+                .add_filter("rust", &["rs", "toml"])
+                .set_directory("/")
+                .pick_file();
+            ui.set_info(files.unwrap().as_path().to_str().unwrap().into());
+           }
+    });
+
+    ui.run()
 }
